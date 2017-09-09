1f675983963772a2f6e458d10baf4093f16ed4f1
Vipin Nambiar Balakrishnan <bvipin@zeomega.com>
2017-09-09 22:35:08 +0530

    Code refactoring

--- utils/date_util.py
+++ utils/date_util.py
@@ -77,6 +77,40 @@ def add_days(current_date, days=1, business_day=False):
     return next_date
 
 
+def add_timedelta(current_date,
+                  time_to_add,
+                  time_left,
+                  start_time=(0, 0, 0),
+                  total_business_hours=timedelta(hours=24)
+                  ):
+    """
+    @current_date: timezone aware datetime object
+    @time_to_add: timedelta object representing the time to be added
+    @time_left: timedelta object representing the available time in the day
+    @start_time: (hour, minute, seconds) - 3 integer Tuple
+    @total_business_hours: timedelta object representing total time in a day
+
+    Returns datetime object calculated by adding the time_to_add to current_date.
+    Considers only the time between start_time and start_time + total_business_hours
+    in a day for calculation.
+    """
+    while time_to_add:
+        if time_to_add < time_left:
+            current_date = current_date + time_to_add
+            break
+        else:
+            next_date = get_next_business_day_time(current_date, start_time)
+            time_to_add = time_to_add - time_left
+            if time_to_add < total_business_hours:
+                current_date = next_date + time_to_add
+                break
+            else:
+                time_left = total_business_hours
+                current_date = next_date
+                continue
+    return current_date
+
+
 def add_hours(current_date,
               hours=1,
               business_day=False,
@@ -89,8 +123,9 @@ def add_hours(current_date,
     @business_day: Consider only business days - Boolean
     @business_hour: Consider only business hour - Boolean
     @start_time: (hour, minute, seconds) - 3 integer Tuple
+    @end_time: (hour, minute, seconds) - 3 integer Tuple
 
-    Returns date and time after adding the specified hours.
+    Returns datetime after adding the specified hours to current_date.
     If business_day flag is True, excludes weekends and holidays for calculation.
     If business_hour flag is True, considers only business hours between start_time and end_time for calculation
     """
@@ -122,46 +157,20 @@ def add_hours(current_date,
             current_date = start_of_day
         else:
             time_left = end_of_day - end_of_day
-        while time_to_add:
-            if time_to_add < time_left:
-                current_date = current_date + time_to_add
-                break
-            else:
-                next_date = get_next_business_day_time(current_date, start_time)
-                time_to_add = time_to_add - time_left
-                if time_to_add < total_business_hours:
-                    current_date = next_date + time_to_add
-                    break
-                else:
-                    time_left = total_business_hours
-                    current_date = next_date
-                    continue
-        return current_date
+        return add_timedelta(current_date, time_to_add, time_left, start_time, total_business_hours)
     elif business_day:
+        start_time = (0, 0, 0)
+        total_business_hours = timedelta(hours=24)
         end_of_day = datetime(current_date.year,
                               current_date.month,
                               current_date.day,
-                              0,
-                              0,
-                              0,
+                              start_time[0],
+                              start_time[0],
+                              start_time[0],
                               tzinfo=current_date.tzinfo
                               ) + timedelta(hours=24)
         time_left = end_of_day - current_date
-        while time_to_add:
-            if time_to_add < time_left:
-                current_date = current_date + time_to_add
-                break
-            else:
-                next_date = get_next_business_day_time(current_date, (0, 0, 0))
-                time_to_add = time_to_add - time_left
-                if time_to_add < timedelta(days=1):
-                    current_date = next_date + time_to_add
-                    break
-                else:
-                    time_left = timedelta(days=1)
-                    current_date = next_date
-                    continue
-        return current_date
+        return add_timedelta(current_date, time_to_add, time_left, start_time, total_business_hours)
     else:
         return current_date + timedelta(hours=hours)
 
@@ -172,7 +181,7 @@ if __name__ == '__main__':
     print add_hours(current_date=central.localize(dt),
                     hours=26.5,
                     business_day=True,
-                    business_hour=False,
+                    business_hour=True,
                     start_time=(10, 0, 0),
                     end_time=(18, 0, 0)
                     )
